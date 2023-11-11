d89dc2d972d3e41250f8b3e7e0a64ff9281d692d
Vaqint <Samuelcollette1223@hotmail.com>
2023-11-11 18:09:55 -0500

    added seperation between results

--- internal/utils.py
+++ internal/utils.py
@@ -63,6 +63,7 @@ def write_q6_ans(file_name, dict_to_print, type_classifier):
 		f.write(str(dict_to_print["weighted"]))
 		f.write("\nVariance of Weighted average: ")
 		f.write(str(dict_to_print["var_weighted"]))
+		f.write("\n" + "-" * 60 + "\n")
 	
 
 
